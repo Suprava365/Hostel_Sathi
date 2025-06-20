6579d660353369da81e7b53744347ad6fc29e77d
Suprava365 <suprava365@gmail.com>
2025-06-20 16:42:17 +0545

    login entity

--- lib/features/login/domain/repository/login_repository.dart
+++ lib/features/login/domain/repository/login_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:hostel_sathi/core/error/failure.dart';
-import 'package:hostel_sathi/features/login/domain/entity/login_entity.dart';
+import 'package:hostel_sathi/features/login/domain/entity/login_enitity.dart';
 
 abstract interface class ILoginRepository {
   Future<Either<Failure, bool>> checkLogin(LoginEntity login);
