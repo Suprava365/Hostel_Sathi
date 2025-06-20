b647db1ffd8de27f1d54465456d9d47c0d407bbc
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-20 16:35:29 +0545

    added local repository and login code

--- lib/app/service_locator/service_locator.dart
+++ lib/app/service_locator/service_locator.dart
@@ -1,13 +1,13 @@
 import 'package:get_it/get_it.dart';
-import 'package:ghar_sewa/core/network/hive_service.dart';
-import 'package:ghar_sewa/features/login/data/data_source/local_datasource/login_local_datasource.dart';
-import 'package:ghar_sewa/features/login/data/repository/local_repository/login_local_repository.dart';
-import 'package:ghar_sewa/features/login/domain/use_case/check_login_usecase.dart';
-import 'package:ghar_sewa/features/login/presentation/view_model/login_view_model.dart';
-import 'package:ghar_sewa/features/register/data/data_source/local_datasource/register_local_datasource.dart';
-import 'package:ghar_sewa/features/register/data/repository/local_repository/register_local_repository.dart';
-import 'package:ghar_sewa/features/register/domain/use_case/add_register_usecase.dart';
-import 'package:ghar_sewa/features/register/presentation/view_model/register_view_model.dart';
+import 'package:hostel_sathi/core/network/hive_service.dart';
+import 'package:hostel_sathi/features/login/data/data_source/local_datasource/login_local_datasource.dart';
+import 'package:hostel_sathi/features/login/data/repository/local_repository/login_local_repository.dart';
+import 'package:hostel_sathi/features/login/domain/use_case/check_login_usecase.dart';
+import 'package:hostel_sathi/features/login/presentation/view_model/login_view_model.dart';
+import 'package:hostel_sathi/features/register/data/data_source/local_datasource/register_local_datasource.dart';
+import 'package:hostel_sathi/features/register/data/repository/local_repository/register_local_repository.dart';
+import 'package:hostel_sathi/features/register/domain/use_case/add_register_usecase.dart';
+import 'package:hostel_sathi/features/register/presentation/view_model/register_view_model.dart';
 
 final serviceLocator = GetIt.instance;
 
---
+++ lib/features/login/data/repository/local_repository/login_local_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:hostel_sathi/core/error/failure.dart';
+import 'package:hostel_sathi/features/login/data/data_source/local_datasource/login_local_datasource.dart';
+import 'package:hostel_sathi/features/login/domain/entity/login_entity.dart';
+import 'package:hostel_sathi/features/login/domain/repository/login_repository.dart';
+import 'package:hostel_sathi/features/login/data/model/login_hive_model.dart';
+
+class LoginLocalRepository implements ILoginRepository {
+  final ILoginLocalDataSource loginLocalDataSource;
+
+  LoginLocalRepository({required this.loginLocalDataSource});
+
+  @override
+  Future<Either<Failure, bool>> checkLogin(LoginEntity login) async {
+    try {
+      final model = LoginHiveModel.fromEntity(login);
+      final isValid = await loginLocalDataSource.checkLogin(model);
+      return Right(isValid);
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+}
